87e342d88095920267bfaccbc65d0cc8cc73e38f
xxnmxx <34981605+xxnmxx@users.noreply.github.com>
2019-05-03 11:04:52 +0900

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install update
-sudo apt install -y tree python3-pip 
+sudo apt install -y tree python3-pip git 
 sudo python3 -m pip install flake8 venv pipenv
 sudo git clone https://github.com/w0rp/ale.git ~/.vim/pack/git-plugins/start/ale
 echo -e set number\\nset nocompatible\\nsyntax enable\\nfiletype plugin indent on\\nautocmd FileType python set omnifunc=syntaxcomplete#Complete\\ncolorscheme slate\\nlet g:ale_sign_column_always = 1\\nlet g:ale_completion_enabled = 1 > .vimrc
